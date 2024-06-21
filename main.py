ae4f4b00985d339e5ab318179d39949848e97551
Assem Ihab <assimehab@gmail.com>
2024-06-21 13:54:36 +0300

    last version

--- main.py
+++ main.py
@@ -32,15 +32,13 @@ if __name__ == "__main__":
     st.title("Image Upload and Processing App")
 
     st.markdown('<h1 class="head">Choose an image...</h1>', unsafe_allow_html=True)
-    uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
 
+    uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
 
     st.markdown('<h1 class="head">Select Model...</h1>', unsafe_allow_html=True)
     st.markdown('<h1 class="desc">Yolov5 Objects365.pt has 365 classes and Yolov8n.pt has 80 classes</h1>', unsafe_allow_html=True)
     model_path = st.selectbox("", ["yolov8n.pt", "yolov5m_Objects365.pt"])
     st.markdown(f'<style>.st-ck select {{ font-size: 18px; }}</style>', unsafe_allow_html=True)
-
-
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image', use_column_width=True)
@@ -63,12 +61,12 @@ if __name__ == "__main__":
                     _, _, _, _, confidence, cls = obj
                     class_name = result.names[cls]
                     st.write(f"{class_name} (confidence: {confidence:.2f})")
-                    end=time.time()
+                end=time.time()
                 st.write(f"Time taken using the V8 model: {end-start} seconds")
             else:
                 names=results.pandas().xyxy[0].name
                 confidence=results.pandas().xyxy[0].confidence
                 for i in range(len(names)):
                     st.write(f"{names[i]} (confidence: {confidence[i]})")
-                    end=time.time()
+                end=time.time()
                 st.write(f"Time taken using the V5 model: {end-start} seconds")
